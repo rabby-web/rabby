92e82eba001b0c35525b217aa8f0d4b5c4d40682
Rabby <rabby.webdeveloper@gmail.com>
2023-01-03 11:12:55 +0600

    faq

--- javascript/script.js
+++ javascript/script.js
@@ -9,3 +9,20 @@ var typed = new Typed(".typing-2",{
     typeSpeed: 80,
     backSpeed: 80,
 });
+
+// FAQ
+const faqs =document.querySelectorAll('.faq');
+faqs.forEach(faq => {
+    faq.addEventListener('click', () =>{
+        faq.classList.toggle('open');
+
+        // change icon
+        const icon = faq.querySelectorAll('faq_icon i');
+        if(icon.className === 'fa-solid fa-plus'){
+            icon.className = 'fa-solid fa-minus';
+        }
+        else {
+            icon.className = 'fa-solid fa-plus';
+        }
+    })
+})
